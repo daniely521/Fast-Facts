ea0ec0b7031088a59ba14f69d364ab8181eba9c1
daniely <fruit.broz@gmail.com>
2024-12-16 15:30:20 +0100

    Added more questions

--- script.js
+++ script.js
@@ -27,7 +27,42 @@ const questions = [
     { question: "What is the 'Friends' coffee shop called?", answer: "Central Perk"},
     { question: "How many planets in our solar system?", answer: "8"},
     { question: "Which mammal can fly?", answer: "bat"},
-    { question: "What is the biggest festival in Brazil", answer: "Carnival"}
+    { question: "What is the biggest festival in Brazil", answer: "Carnival"},
+    { question: "How many continents are there", answer: "7"},
+    { question: "Who developed the theory of relativity", answer: "Einstein"},
+    { question: "What is the square root of 144", answer: "12"},
+    { question: "What year did WWII end?", answer: "1945"},
+    { question: "What country has the most population", answer: "India"},
+    { question: "How many players on a soccer team?", answer: "11"},
+    { question: "What is the name of the hobbit in 'The Hobbit'", answer: "Bilbo Baggins"},
+    { question: "Which movie has the quote 'I'll be back'", answer: "Terminator"},
+    { question: "Who sang shape of you?", answer: "Ed Sheeran"},
+    { question: "Which country is known for its sushi", answer: "Japan"},
+    { question: "What is the most used programming language?", answer: "Javascript"},
+    { question: "What animal is known for its mimicking sounds?", answer: "parrot"},
+    { question: "What is Spongebob's snail called", answer: "Gary"},
+    { question: "Which language has the most native speakers?", answer: "Chinese"},
+    { question: "Which word is spelt incorrectly in every dictionary", answer: "incorrectly"},
+    { question: "What is the longest running animated TV show?", answer: "Simpsons"},
+    { question: "What is the hottest planet in the solar system?", answer: "Venus"},
+    { question: "What is the baby kangaroo called?", answer: "Joey"},
+    { question: "Which country celebrates thanksgiving in October?", answer: "Canada"},
+    { question: "What is the largest ocean in the world?", answer: "Pacific"},
+    { question: "What do plants absorb during photosynthesis?", answer: "carbon dioxide"},
+    { question: "What is pi to 2 decimal places?", answer: "3.14"},
+    { question: "What empire was ruled by Julius Caesar", answer: "Roman"},
+    { question: "Which river runs through egypt", answer: "Nile"},
+    { question: "What sports use 'Love' for 0 points?", answer: "Tennis"},
+    { question: "Which song is most streamed on spotify?", answer: "Blinding Lights"},
+    { question: "What fruit is the 'king of fruits'?", answer: "Durian"},
+    { question: "What does HTTP stand for?", answer: "Hypertext Transfer Protocol"},
+    { question: "What is the tallest tree species?", answer: "redwood"},
+    { question: "What is the name of batman's butler?", answer: "Alfred"},
+    { question: "What is the national animal of Scotland?", answer: "Unicorn"},
+    { question: "In which city is Strangers Things set?", answer: "Hawkins"},
+    { question: "Which planet has the largest rings?", answer: "Saturn"},
+    { question: "What is a group of crows called", answer: "murder"},
+    { question: "What is the name of the 'Jewish festival of lights'", answer: "Hannukah"},
 ];
 
 const timerElement = document.getElementById("timer");
@@ -53,6 +88,7 @@ function startGame() {
     answerElement.disabled = false;
     answerElement.value = "";
     scoreElement.textContent = `Score: ${score}`;
+    scoreElement.classList.remove('hidden')
     startButton.classList.add("hidden")
     answerElement.classList.remove("hidden")
     loadQuestion();
@@ -101,6 +137,7 @@ function endGame() {
     startButton.textContent = "Restart";
     answerElement.classList.add("hidden")
     currentQuestionIndex++;
+    scoreElement.classList.add("hidden")
 }
 
 startButton.addEventListener('click', startGame);
